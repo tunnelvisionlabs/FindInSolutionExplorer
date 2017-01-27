c455670439917174e25ed2e935d5740e10f86bf0
Sam Harwell <sam@tunnelvisionlabs.com>
2017-01-27 06:15:35 -0600

    Update assembly info for release 3.1.0

--- Tvl.VisualStudio.FindInSolutionExplorer/Properties/AssemblyInfo.cs
+++ Tvl.VisualStudio.FindInSolutionExplorer/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.1.0.0")]
 [assembly: AssemblyFileVersion("3.1.0.0")]
-[assembly: AssemblyInformationalVersion("3.1.0-dev")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
